c4b451499cc7c4529d1a0791f2f9d26f5b265f3c
tigran9910 <90608993+tigran9910@users.noreply.github.com>
2022-03-27 20:24:10 +0200

    information update

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -12,36 +12,36 @@ export default function Resume() {
           <div className="section summary">
             <h3 className="section-name">Summary</h3>
             <p>
-              Business Informatics student with experience in accounting and
-              audit. Finished school in Moscow at the age of 16 with 2 golden
-              medals and started living alone and studying in Germany. Learn Web
-              Development and Machine Learning. Have created several websites
-              and web apps, several ML projects with Python and a new very
-              simple programming language on Typescript.
+              Business Informatics student working as a part-time Web Developer
+              at BASF. Learn about Machine Learning in free time.
             </p>
           </div>
-          <div className="section software">
-            <h3 className="section-name">Software</h3>
+          <div className="section experience">
+            <h3 className="section-name">Experience</h3>
             <ul>
               <li>
-                <b>Languages:</b> Python, JavaScript, C#, Java, CSS, HTML, R
+                <p className="exp-job">Web Development Working Student</p>
+                <p className="exp-date">11/2021 - today</p>
+                <p className="exp-firm">BASF, Berlin Germany</p>
+                <p className="exp-description">
+                  Updating one of the biggest internal web applications of BASF
+                  Global Shared Services Centers (including core functions).
+                  Writing documentation, test cases and testing application.
+                  Created a general frontend component for all web applications
+                  of internal GSS portal using external library.
+                </p>
               </li>
               <li>
-                <b>Technologies:</b> Linux, NodeJS, ExpressJS, TypeScript,
-                React, Gatsby, Docker
+                <p className="exp-job">Audit Intern</p>
+                <p className="exp-date">08/2019 - 10/2019 (2m)</p>
+                <p className="exp-firm">Deloitte, Moscow Russia</p>
               </li>
               <li>
-                <b>Source Control:</b> Git
-              </li>
-              <li>
-                <b>Databases:</b> MySQL, MongoDB
-              </li>
-              <li>
-                <b>IDE:</b> Visual Studio Code, IntelliJ IDEA
-              </li>
-              <li>
-                <b>Other:</b> Power Bi, Microsoft Office, Excel (VBA), DATEV,
-                SAP
+                <p className="exp-job">Accounting Working Student</p>
+                <p className="exp-date">11/2018 - 07/2019 (9m)</p>
+                <p className="exp-firm">
+                  Schürmann Steuerberatungsgesellschaft mbH, Berlin Germany
+                </p>
               </li>
             </ul>
           </div>
@@ -56,34 +56,17 @@ export default function Resume() {
         </div>
 
         <div className="right">
-          <div className="section experience">
-            <h3 className="section-name">Experience</h3>
-            <ul>
-              <li>
-                <p className="exp-job">Audit Intern</p>
-                <p className="exp-date">08/2019 - 10/2019 (2m)</p>
-                <p className="exp-firm">Deloitte, Moscow, Russia</p>
-              </li>
-              <li>
-                <p className="exp-job">Accounting Working Student</p>
-                <p className="exp-date">11/2018 - 07/2019 (9m)</p>
-                <p className="exp-firm">
-                  Schürmann Steuerberatungsgesellschaft mbH, Berlin, Germany
-                </p>
-              </li>
-            </ul>
-          </div>
           <div className="section education">
             <h3 className="section-name">Education</h3>
             <ul>
               <li>
                 <h4>Free University of Berlin</h4>
                 <p className="edu-name">
-                  B.Sc. Business Administration (Business Informatics
-                  specialization)
+                  B.Sc. Business Administration (majoring in Business
+                  Informatics)
                 </p>
                 <p className="edu-info">
-                  2.7 GPA (good) - German grading system
+                  2.8 GPA (satisfactory) - German grading system
                 </p>
                 <p className="edu-info">
                   <b>Courses:</b> OOP (Java), IT for Economists (C#)
@@ -97,6 +80,30 @@ export default function Resume() {
                 <p className="edu-info">
                   1.1 GPA (excellent) - German grading system
                 </p>
+              </li>
+            </ul>
+          </div>
+          <div className="section software">
+            <h3 className="section-name">Software</h3>
+            <ul>
+              <li>
+                <b>Languages:</b> Python, JavaScript, C#, Java, CSS, HTML, R
+              </li>
+              <li>
+                <b>Technologies:</b> Linux, NodeJS, ExpressJS, TypeScript,
+                Groovy, React, Gatsby, React Native, Docker
+              </li>
+              <li>
+                <b>Source Control:</b> Git, Subversion
+              </li>
+              <li>
+                <b>Databases:</b> Oracle SQL, MySQL, MongoDB
+              </li>
+              <li>
+                <b>IDE:</b> Visual Studio Code, IntelliJ IDEA
+              </li>
+              <li>
+                <b>Other:</b> Azure DevOps, Jira, DATEV, SAP
               </li>
             </ul>
           </div>
